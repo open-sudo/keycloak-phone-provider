6f5bc49d3632c8d42e275e5e662f821da5478513
open-sudo <open@sudo>
2021-08-25 11:30:36 -0400

    deploy keycloak

--- openshift/deploy.sh
+++ openshift/deploy.sh
@@ -1,6 +1,7 @@
+oc create configmap keycloak-config --from-file=configmap.yaml 
 oc create -f buildconfig.yaml
-oc create imagestream nginx-hello-world
-oc start-build nginx-hello-world --from-dir=./ --follow
-oc create -f deployment.yaml
-oc create -f clusterip.yaml
-oc create -f route.yaml
+oc create imagestream keycloak-appui
+oc start-build keycloak-appui --from-dir=./ --follow
+#oc create -f deployment.yaml
+#oc create -f clusterip.yaml
+#oc create -f route.yaml
