021dfaedf9e58710c82b9131e119875b78c2f2f8
open-sudo <open@sudo>
2021-08-27 22:11:36 -0400

    keycloak deployment tempates

--- openshift/deploy.sh
+++ openshift/deploy.sh
@@ -1,7 +1,7 @@
-oc create configmap keycloak-config --from-file=configmap.yaml 
+#oc create configmap keycloak-config --from-file=configmap.yaml 
 oc create -f buildconfig.yaml
 oc create imagestream keycloak-appui
 oc start-build keycloak-appui --from-dir=./ --follow
-#oc create -f deployment.yaml
-#oc create -f clusterip.yaml
-#oc create -f route.yaml
+oc create -f deployment.yaml
+oc create -f clusterip.yaml
+oc create -f route.yaml
